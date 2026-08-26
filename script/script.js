5645926ab8beba97b6e3d6161d905e710e11656b
Guilherme Souza <257373090+Sturion00@users.noreply.github.com>
2026-08-26 00:19:32 -0300

    Na luta....

--- script/script.js
+++ script/script.js
@@ -1,5 +1,9 @@
-var cityName = document.querySelector('div#cityName')
-
+var cityName = document.querySelector('input#cityName')
 const apiKey = '0e7292031e57d5fd6ec254f6f07c0676'
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cityName)}&appid=${apiKey}&units=metric&lang=pt_br`
 
+async function chamarApi() {
+    const resp = await fetch(apiUrl)
+}
+
+chamarApi()
