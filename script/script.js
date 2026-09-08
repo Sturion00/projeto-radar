4a6b7ddbce2a01af7c77c344723f540553d1bd85
Guilherme Souza <257373090+Sturion00@users.noreply.github.com>
2026-09-08 17:26:28 -0300

    Primeira chamada de API :)

--- script/script.js
+++ script/script.js
@@ -1,9 +1,12 @@
-var cityName = document.querySelector('input#cityName')
 const apiKey = '0e7292031e57d5fd6ec254f6f07c0676'
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cityName)}&appid=${apiKey}&units=metric&lang=pt_br`
+const url = `https://api.openweathermap.org/data/2.5/weather?q=marialva&appid=${apiKey}&lang=pt-br&units=metric`
 
-async function chamarApi() {
-    const resp = await fetch(apiUrl)
+async function apiCall() {
+    const resp = await fetch(url)
+    if (resp.status === 200) {
+        const obj = await resp.json()
+        console.log(obj)
+    }
 }
 
-chamarApi()
+apiCall()
